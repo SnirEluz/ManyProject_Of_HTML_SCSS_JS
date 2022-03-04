af141be350df12951846d5d4ba5ecfaab89c37ab
snireluz <snireluzttt@gmail.com>
2022-03-04 10:48:47 +0200

    first commit

---
+++ NFT/mission.js
@@ -0,0 +1,6 @@
+const paretDiv = document.createElement('div')
+const img = document.createElement('img')
+const name = document.createElement('name')
+const del = document.createElement('btn')
+
+fun
